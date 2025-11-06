872c385490bd435169228e536f669d452c1292d9
Nicolás Chabot <nicochabmelo@gmail.com>
2025-11-06 14:05:13 -0300

    Update app_demo.py

--- app_demo.py
+++ app_demo.py
@@ -7,7 +7,7 @@ from bolt_exception import BoltException
 # Caminho da imagem do bot
 st.image("bolt.png", width=90)
 
-st.title(st.image("bolt.png", width=90) "Agente Bolt - Sistema de Materiais")
+st.title("bolt.png" "Agente Bolt - Sistema de Materiais")
 
 # Inicialização de estado
 if "state" not in st.session_state:
@@ -91,6 +91,7 @@ if user_input:
     except Exception as e:
         st.error(f"**Erro inesperado:** {e}")
         st.session_state.state = "menu"
+
 
 
 
